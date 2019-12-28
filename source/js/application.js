06497309be9106394cadd5144695b4683ed4fa6d
Eric Gardner <gardner.ec@gmail.com>
2019-12-27 17:05:25 -0800

    Minor style changes
    * Disable progress bar
    * Update theme variables:
      Switch theme to "classic" mode
      Set accent color to the one on SJMA website
      Enable some of the default fonts
    * Re-enable menu JS
    * Disable navbar padding
    * Address CSS linter rule violations

--- source/js/application.js
+++ source/js/application.js
@@ -1,4 +1,41 @@
-// TBD
+import $ from 'jquery';
 
 // CSS
 import "../css/application.scss";
+
+// Essential functionality
+
+/**
+ * toggleMenu
+ * @description Show/hide the menu UI by changing CSS classes and Aria status.
+ * This function is bound to the global window object so it can be called from
+ * templates without additional binding.
+ */
+window[ 'toggleMenu' ] = () => {
+  let menu = document.getElementById('site-menu');
+  let menuAriaStatus = menu.getAttribute('aria-expanded');
+
+	menu.classList.toggle(
+		'is-expanded',
+		!menu.classList.contains('is-expanded')
+	);
+
+	if (menuAriaStatus === 'true') {
+		$('.side-by-side > .quire-entry__image-wrap > .quire-entry__image').removeClass('menu_open');
+		menu.setAttribute('aria-expanded', 'false');
+	} else {
+    $('.side-by-side > .quire-entry__image-wrap > .quire-entry__image').addClass('menu_open');
+    menu.setAttribute('aria-expanded', 'true');
+  }
+};
+
+/**
+ * globalSetup
+ * @description Initial setup on first page load.
+ */
+function globalSetup() {
+	let container = document.getElementById("container");
+	container.classList.remove("no-js");
+}
+
+globalSetup();
